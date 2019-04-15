88ab563c2aa82c08de339323f7f6ae4e0abfada2
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-04-15 17:33:35 +0200

    suite du tp3

---
+++ src/main/java/dev/sgp/entite/Collaborateur.java
@@ -0,0 +1,18 @@
+package dev.sgp.entite;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+public class Collaborateur {
+
+	private String matricule;
+	private String nom;
+	private String prenom;
+	private LocalDate dateDeNaissance;
+	private String adresse;
+	private String numeroDeSecuriteSociale;
+	private String email;
+	private String photo;
+	private ZonedDateTime dateHeureCreation;
+
+}
