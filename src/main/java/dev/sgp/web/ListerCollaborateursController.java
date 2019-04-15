8aec22e2883b70a55acfc34fedcd3d287b296e4c
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-04-15 17:01:56 +0200

    v3-premiere-page-jsp

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,20 +21,24 @@ public class ListerCollaborateursController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
+		req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
 		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
-		;
-		// recupere la valeur d'un parametre dont le nom est avecPhoto
-		String avecPhotoParam = req.getParameter("avecPhoto");
-
-		// recuperer la valeur d'un parametre dont le nom est departement
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-
-		// resp.getWriter().write("Hello ListerCollaborateursController");
-
-		// code HTML ecrit dans le corps de la reponse
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>" + "<ul>" + "<li>avecPhoto=" + avecPhotoParam
-				+ "</li>" + "<li>departement=" + departementParam + "</li>" + "</ul>");
+		/*
+		 * // recupere la valeur d'un parametre dont le nom est avecPhoto String
+		 * avecPhotoParam = req.getParameter("avecPhoto");
+		 * 
+		 * // recuperer la valeur d'un parametre dont le nom est departement String
+		 * departementParam = req.getParameter("departement");
+		 * resp.setContentType("text/html");
+		 * 
+		 * // resp.getWriter().write("Hello ListerCollaborateursController");
+		 * 
+		 * // code HTML ecrit dans le corps de la reponse
+		 * resp.getWriter().write("<h1>Liste des collaborateurs</h1>" + "<ul>" +
+		 * "<li>avecPhoto=" + avecPhotoParam + "</li>" + "<li>departement=" +
+		 * departementParam + "</li>" + "</ul>");
+		 * 
+		 */
 	}
 
 }
